6e00ec933f04530be23f74350796d4f44c362cd9
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2026-03-28 20:29:28 +0200

    Add files via upload

---
+++ RedditAnalyzer/RedditAnalyzer/Models/RequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RedditAnalyzer.Models;
+
+public class RequestModel
+{
+    public List<SubredditItem> Items { get; set; }
+    public int Limit { get; set; }
+}
+
+public class SubredditItem
+{
+    public string Subreddit { get; set; }
+    public List<string> Keywords { get; set; }
+}
